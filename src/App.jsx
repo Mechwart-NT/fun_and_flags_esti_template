06af9eab95be76c447ed4aa398603976b4e8b7a5
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-05-05 17:46:48 +0200

    filter by region works

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { useEffect, useState } from "react"
+import CountryCard from "./components/CountryCard"
 
 function App() {
- const [isDark, setIsDark] = useState(true)
+  const [isDark, setIsDark] = useState(true)
+  const [countries, setCountries] = useState([])
+
+  const regions = ["All","Africa", "Americas","Asia","Europe","Oceania","Polar"]
+  const [selectedRegion, setSelectedRegion] = useState(regions[0])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(response => response.json())
+    .then(data => setCountries(data))
+  },[])
 
   useEffect(()=>{
     document.body.className = isDark ? "dark" : "light"
   }, [isDark])
+
+  const filterByRegion = (li) => {
+    if(selectedRegion === "All") return li
+
+    return li.filter(element => element.region === selectedRegion)
+
+    // return li.filter((element) => {
+    //   if(element.region === selectedRegion)
+    //     return element
+    // })
+  }
 
   return (
     <>
     <button onClick={()=>setIsDark(prev => !prev)}>
       {isDark ? "SETÉT" : "VILÁGOS"}
     </button>
+
+    <select value={selectedRegion} onChange={(e)=>{ setSelectedRegion(e.target.value) }}>
+      {regions.map(regio => <option value={regio}>{regio}</option>)}
+    </select>
+
+    <section className="cardWrapper">
+      {filterByRegion(countries).map(country => <CountryCard {...country} />)}
+    </section>
     </>
   )
 }
