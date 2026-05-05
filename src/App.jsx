89767b9de446907eb144b87f5eef74c6039df70b
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-05-05 16:33:29 +0200

    dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react"
+
 function App() {
+ const [isDark, setIsDark] = useState(true)
+
+  useEffect(()=>{
+    document.body.className = isDark ? "dark" : "light"
+  }, [isDark])
 
   return (
     <>
+    <button onClick={()=>setIsDark(prev => !prev)}>
+      {isDark ? "SETÉT" : "VILÁGOS"}
+    </button>
     </>
   )
 }
